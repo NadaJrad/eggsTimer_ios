81732b1830c21b2ddfb6c2fdcac8987926387e95
NadaJrad <nadajradbenaicha@gmail.com>
2025-05-12 12:05:24 +0200

    changing the code using dicionary

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,15 +1,13 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggsTimes = ["Soft":5, "Medium":7, "Hard":12]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        let result = eggsTimes[hardness]!
+        print (result)
+        
+    }
 }
